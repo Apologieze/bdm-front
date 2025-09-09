825c4e52eb249bdd21a9e9dc98a3e3dcbd4d0639
Apo <apoloing2@gmail.com>
2025-09-09 11:19:58 +0200

    poupa

--- src/lib/components/SecondSection.svelte
+++ src/lib/components/SecondSection.svelte
@@ -1,6 +1,6 @@
 <section class="h-screen w-full bg-blue-900 flex justify-center items-center snap-start">
   <div class="text-center text-white">
     <h1 class="text-4xl font-bold">Second Section</h1>
-    <p class="mt-4 text-xl">This is the next page you snap to.</p>
+    <p class="mt-4 text-xl">This is the next page you snap to POUPA.</p>
   </div>
 </section>
